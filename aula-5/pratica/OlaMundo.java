5119ff7ed05de4499f0bcc03309b83ca86992160
Allyson Brandão (VPN) <allyson.brandao@venhapranuvem.com.br>
2020-10-21 09:26:55 -0300

    Praticando Hello World

---
+++ aula-5/pratica/OlaMundo.java
@@ -0,0 +1,17 @@
+package pratica;
+
+class OlaMUndo{
+    public static void main(String[]args){
+        System.out.println("OLa Mundo");
+    }
+}
+
+/*
+
+- inicie no terminal com os seguintes comandos
+
+ - javac OlaMundo.java (Este comando irá criar o arquivo OlaMUndo.class .)
+
+ - java OlaMundo.java (Este comando printar no terminal Ola Mundo.)
+
+*/
