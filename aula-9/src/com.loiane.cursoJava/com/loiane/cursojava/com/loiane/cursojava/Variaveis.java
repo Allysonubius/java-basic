646da7acb0bbd5230b2b7f185f405bae183bff63
Allyson Brandão (VPN) <allyson.brandao@venhapranuvem.com.br>
2020-10-22 09:35:10 -0300

    Introdução a variaveis

---
+++ aula-9/src/com.loiane.cursoJava/com/loiane/cursojava/com/loiane/cursojava/Variaveis.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava;
+
+public class Variaveis{
+    public static void main(String[] args){
+        //Convenção Java
+        int idade ;
+        
+        String name;
+
+        String nameMyDog;
+
+        //Inserindo o valores
+        idade = 20;
+
+        name = "Kokinha";
+
+        nameMyDog = "bolinha";
+
+        //Println
+        System.out.println("Idade " + idade);
+        System.out.println("name " + name);
+        System.out.println("Dog " + nameMyDog);
+    }
+}
