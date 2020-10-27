31fd687b98b8f79026aa11d692cf740a1db2f682
Allyson Brandão (VPN) <allyson.brandao@venhapranuvem.com.br>
2020-10-26 21:03:23 -0300

    Loop For

---
+++ Aula-17/src/com.loaine.java/LoopFor.java
@@ -0,0 +1,11 @@
+package com.loaine.cursojava;
+
+public class LoopFor{
+
+    public static void main(String{} args){
+
+        for(int i =0; i < 5; i ++){
+            System.out.println("i tem o valor : " + i);
+        }
+    }
+}
