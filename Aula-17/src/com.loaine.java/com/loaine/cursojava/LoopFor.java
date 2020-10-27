e23f06942bde19edfa4541afef1c95fbfc8a84e6
Allyson Brandão (VPN) <allyson.brandao@venhapranuvem.com.br>
2020-10-26 21:03:34 -0300

    Loop for

--- Aula-17/src/com.loaine.java/com/loaine/cursojava/LoopFor.java
+++ Aula-17/src/com.loaine.java/com/loaine/cursojava/LoopFor.java
@@ -2,7 +2,7 @@ package com.loaine.cursojava;
 
 public class LoopFor{
 
-    public static void main(String{} args){
+    public static void main(String[] args){
 
         for(int i =0; i < 5; i ++){
             System.out.println("i tem o valor : " + i);
